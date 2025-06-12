9b201f8a62925edf63e5e24104216c038ed69215
VinzJ <kejoxz@gmail.com>
2025-06-13 00:57:56 +0800

    feat: initialize vue 3 project with core plugin

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HomeView from "./views/HomeView.vue";
+</script>
+
+<template>
+  <div class="flex justify-center min-w-full min-h-screen p-4 bg-white-100">
+    <HomeView />
+  </div>
+</template>
