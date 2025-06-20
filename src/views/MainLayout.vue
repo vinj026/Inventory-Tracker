d3b4a4519d14fc68813352a218dafbceecb8104b
VinzJ <kejoxz@gmail.com>
2025-06-21 03:00:43 +0800

    feat: add function to store data in localstorage

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -1,5 +1,15 @@
 <script setup>
 import Sidebar from "@/components/layout/Sidebar.vue";
+import { onMounted } from "vue";
+import { useInventoryStore } from "@/stores/useInventoryStore.js";
+
+const inventoryStore = useInventoryStore();
+
+onMounted(() => {
+  inventoryStore.loadFromLocalStorage();
+  console.log(inventoryStore.productInventory.savedCategory);
+  console.log(inventoryStore.productInventory.productList);
+});
 </script>
 <template>
   <div class="grid grid-cols-[auto_1fr] w-full min-h-screen bg-white-100">
