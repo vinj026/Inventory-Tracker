d90cc31ea4664a90ffe6d9f5c8282495e8408546
Kevin <kejoxz@gmail.com>
2025-06-28 05:15:24 +0800

    feat(dashboard): add empty state

--- src/components/spesific/LowStockTable.vue
+++ src/components/spesific/LowStockTable.vue
@@ -5,24 +5,28 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="overflow-x-auto  border border-white-400 rounded-xl">
-    <table class="min-w-full text-sm text-left">
-      <thead class="bg-white-400 text-black-300 font-bold uppercase">
-        <tr>
-          <th class="p-3">Product</th>
-          <th class="p-3">SKU</th>
-          <th class="p-3">Category</th>
-          <th class="p-3">Quantity</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in props.data" :key="index" class="border-t border-white-400 text-black-400">
-          <td class="p-3">{{ item.productName }}</td>
-          <td class="p-3">{{ item.SKU }}</td>
-          <td class="p-3">{{ item.category }}</td>
-          <td class="p-3 text-orange-900 font-bold">{{ item.quantity }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="flex w-full h-full justify-center items-center">
+    <div v-if="data.length !== 0" class="overflow-x-auto border border-white-400 rounded-xl">
+      <table class="min-w-full text-sm text-left">
+        <thead class="bg-white-400 text-black-300 font-bold uppercase">
+          <tr>
+            <th class="p-3">Product</th>
+            <th class="p-3">SKU</th>
+            <th class="p-3">Category</th>
+            <th class="p-3">Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in props.data" :key="index" class="border-t border-white-400 text-black-400">
+            <td class="p-3">{{ item.productName }}</td>
+            <td class="p-3">{{ item.SKU }}</td>
+            <td class="p-3">{{ item.category }}</td>
+            <td class="p-3 text-orange-900 font-bold">{{ item.quantity }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <p v-else class="text-black-50">No Transaction yet</p>
   </div>
+
 </template>
--- src/components/spesific/TransactionTable.vue
+++ src/components/spesific/TransactionTable.vue
@@ -5,24 +5,27 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="overflow-x-auto  border border-white-400 rounded-xl">
-    <table class="min-w-full text-sm text-left">
-      <thead class="bg-white-400 text-black-300 font-bold uppercase">
-        <tr>
-          <th class="p-3">Date</th>
-          <th class="p-3">SKU</th>
-          <th class="p-3">Type</th>
-          <th class="p-3">Quantity</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in props.data" :key="index" class="border-t border-white-400 text-black-400">
-          <td class="p-3">{{ new Date(item.date).toLocaleDateString() }}</td>
-          <td class="p-3">{{ item.SKU }}</td>
-          <td class="p-3 capitalize">{{ item.type }}</td>
-          <td class="p-3">{{ item.quantity }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="flex w-full h-full justify-center items-center">
+    <div class="overflow-x-auto  border border-white-400 rounded-xl" v-if="data.length > 0">
+      <table class="min-w-full text-sm text-left">
+        <thead class="bg-white-400 text-black-300 font-bold uppercase">
+          <tr>
+            <th class="p-3">Date</th>
+            <th class="p-3">SKU</th>
+            <th class="p-3">Type</th>
+            <th class="p-3">Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in props.data" :key="index" class="border-t border-white-400 text-black-400">
+            <td class="p-3">{{ new Date(item.date).toLocaleDateString() }}</td>
+            <td class="p-3">{{ item.SKU }}</td>
+            <td class="p-3 capitalize">{{ item.type }}</td>
+            <td class="p-3">{{ item.quantity }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <p v-else class="text-black-50">No Transaction yet</p>
   </div>
 </template>
